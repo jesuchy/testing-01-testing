1d6660839877d91fd35c9be3624ef0ed055bf13e
jesuchy <jcharrymeza1@gmail.com>
2025-12-04 15:33:32 -0500

    testing 02

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.get('/', (req, res) => res.send('Hello word'))
+
 app.listen(process.env.PORT || 3000);
 
 console.log(`server init in port: ${process.env.PORT || '3000'}`);
